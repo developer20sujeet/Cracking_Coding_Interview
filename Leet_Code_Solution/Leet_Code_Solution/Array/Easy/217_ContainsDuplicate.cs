5292ca92ca52cdc714e44de701ae4ff987bace48
learn360sujeet <121271779+learn360sujeet@users.noreply.github.com>
2023-03-28 14:12:18 -0500

    Leet_Code

---
+++ Leet_Code_Solution/Leet_Code_Solution/Array/Easy/217_ContainsDuplicate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leet_Code_Solution.Array.Easy
+{
+    internal class _217_ContainsDuplicate
+    {
+        /// <summary>
+        /// https://leetcode.com/problems/contains-duplicate/
+        /// </summary>
+        public bool ContainsDuplicate(int[] nums)
+        {
+
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+
+                if (dic.ContainsKey(nums[i]))
+                {
+                    return true;
+                }
+
+                dic[nums[i]] = 1;
+
+            }
+
+            return false;
+
+
+        }
+
+    }
+}
---
+++ Leet_Code_Solution/Leet_Code_Solution/Program.cs
@@ -0,0 +1,7 @@
+﻿
+
+using Leet_Code_Solution.Array.Easy;
+
+
+int[] arr = { 1, 2, 3, 1 };
+new _217_ContainsDuplicate().ContainsDuplicate(arr);
